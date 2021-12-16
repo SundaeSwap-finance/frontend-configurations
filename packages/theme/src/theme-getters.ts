4d474316503c823ed18dca7ded0a1906d132e8df
akratzel <andreas.kratzel@gmail.com>
2021-12-16 11:04:07 +0100

    fix(theme): `getFromTheme` proper types and simplification

--- packages/theme/src/theme-getters.ts
+++ packages/theme/src/theme-getters.ts
@@ -2,39 +2,23 @@ import { path } from 'ramda';
 import { css, CSSObject, SimpleInterpolation } from 'styled-components';
 import { ThemeType } from './theme';
 
-type THEME_INDEXES = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
-
-const getVariant = (x: THEME_INDEXES): string => `-${x}`;
-
-export const getTheme =
-  (keys: [keyof ThemeType, string]) =>
-  (props): SimpleInterpolation =>
-    path(['theme', ...keys], props);
-
 /**
- * Takes in the desired animation based on the available values in our theme and returns the concatenated variable string.
- *
- * @param animation
- * @param index
- * @returns `--<animation-name>
- */
-export const animation = (animationName: keyof ThemeType['animations']) => getTheme(['animations', `${animationName}`]);
-
-/**
- * Takes in the desired color based on the available values in our theme and returns the concatenated variable string.
+ * Dynamically gets the desired values from the theme declaration.
  *
  * @example
  * const Button = styled.div`
- *   background-color: ${color('blue', 2)};
+ *   background-color: ${getFromTheme("colors", "--blue-2")};
  * `
- * --> is equal to: background-color: ${({ theme }) => theme.colors[--blue-2]};
+ * --> is equal to: background-color: ${({ theme }) => theme.colors["--blue-2"]};
  *
- * @param color
- * @param index
- * @returns `--<color>-<index>
+ * @param themeKey: keyof ThemeType
+ * @param themeProperty keyof ThemeType[themeProperty]
+ * @returns `--<themeProperty>-<index>
  */
-export const color = (color: keyof ThemeType['colors'], index: THEME_INDEXES) =>
-  getTheme(['colors', `--${color}${getVariant(index)}`]);
+export const getFromTheme =
+  <T extends keyof ThemeType>(themeProperties: [T, keyof ThemeType[T]]) =>
+  (props: ThemeType): SimpleInterpolation =>
+    path(['theme', ...(themeProperties as string[])], props);
 
 /**
  * This function lets you use your media query dynamically across the consumer application.
@@ -51,7 +35,7 @@ export const color = (color: keyof ThemeType['colors'], index: THEME_INDEXES) =>
  * @returns
  */
 export const mediaQuery = (mediaQuery: keyof ThemeType['media'], styles: CSSObject) => css`
-  @media all and ${getTheme(['media', `${mediaQuery}`])} {
+  @media all and ${getFromTheme(['media', `${mediaQuery}`])} {
     ${styles};
   }
 `;
