f291c683378c6eafe5b4d6154b04d279dc58e832
Calvin Koepke <hello@calvinkoepke.com>
2022-05-17 16:34:13 -0600

    chore: plugins

--- packages/eslint-config/src/index.ts
+++ packages/eslint-config/src/index.ts
@@ -1,9 +1,26 @@
 import { rules } from "./rules";
 
+const hasNext = !!process.env['npm_package_dependencies_next'];
+const hasReact = !!process.env['npm_package_dependencies_react'];
+const hasJest = !!process.env['npm_package_dependencies_jest'];
+
+const pluginList = ["import", "@typescript-eslint", "prettier"];
+const extendList = ["prettier"];
+
+if (hasJest) {
+  pluginList.push("jest");
+}
+
+if (hasNext) {
+  extendList.push("next");
+} else if (hasReact) {
+  extendList.push("react");
+}
+
 export = {
   parser: "@typescript-eslint/parser",
-  plugins: ["jest", "import", "@typescript-eslint", "prettier"],
-  extends: ["next", "prettier"],
+  plugins: pluginList,
+  extends: extendList,
   env: {
     browser: true,
     commonjs: true,
