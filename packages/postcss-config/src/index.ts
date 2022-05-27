2e9b87194c21ddeefaae217553a7a4f5d8926a21
Miklos Suveges <miklos.suveges@gmail.com>
2022-05-27 21:48:51 +0200

    feat(PostCSS): add purgecss options

--- packages/postcss-config/src/index.ts
+++ packages/postcss-config/src/index.ts
@@ -3,25 +3,33 @@ const glob = require("glob");
 const purgecss = require("@fullhuman/postcss-purgecss");
 const normalize = require("postcss-normalize");
 
-export const getConfig = (useScss: boolean = true) => ({
+export type TPostCSSConfigFactoryProps = {
+  useScss?: boolean;
+  purgeCssContent?: string[];
+};
+
+export const getConfig = ({
+  useScss = true,
+  purgeCssContent = [],
+}: TPostCSSConfigFactoryProps = {}) => ({
   ident: "postcss",
   syntax: useScss ? "postcss-scss" : "postcss-safe-parser",
   plugins: [
     "postcss-preset-env",
     normalize(),
+    require("tailwindcss"),
     process.env.NODE_ENV === "production"
       ? purgecss({
+          defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
           content: [
+            ...purgeCssContent,
             path.join(process.cwd(), "./dist/index.html"), //to match index.html
             ...glob.sync(
               `${path.join(process.cwd(), "src")}/**/*.tsx`, //to match React JSX files in src/components/*.jsx
-              {
-                nodir: true,
-              }
+              { nodir: true }
             ),
           ],
         })
       : null,
-    require("tailwindcss"),
   ],
 });
