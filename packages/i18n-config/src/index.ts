640b440dcf0053dddee8d86ee53b460327e6bda1
akratzel <andreas.kratzel@gmail.com>
2022-06-21 18:16:58 +0200

    chore(i18n-config): remove languages file

--- packages/i18n-config/src/index.ts
+++ packages/i18n-config/src/index.ts
@@ -1,7 +1,64 @@
 import i18nextPackage, { InitOptions } from "i18next";
 import { initReactI18next } from "react-i18next";
 
-import { getSupportedLngs, TSupportedLocale } from "./languages";
+type TSupportedLocale = {
+  locale: string;
+  localeString: string;
+};
+
+// default languages based on current dex v1
+const DEFAULT_LANGUAGES: TSupportedLocale[] = [
+  {
+    locale: "ar",
+    localeString: "عربي",
+  },
+  {
+    locale: "de",
+    localeString: "Deutsch",
+  },
+  {
+    locale: "en",
+    localeString: "English",
+  },
+  {
+    locale: "es",
+    localeString: "Español",
+  },
+  {
+    locale: "fr",
+    localeString: "Français",
+  },
+  {
+    locale: "it",
+    localeString: "Italiano",
+  },
+  {
+    locale: "ja",
+    localeString: "日本語",
+  },
+  {
+    locale: "nl",
+    localeString: "Nederlands",
+  },
+  {
+    locale: "pt",
+    localeString: "Português",
+  },
+  {
+    locale: "ru",
+    localeString: "Русский",
+  },
+  {
+    locale: "th",
+    localeString: "ภาษาไทย",
+  },
+];
+
+const getSupportedLngs = (supportedLngs?: TSupportedLocale[]) => {
+  const langs = supportedLngs ?? DEFAULT_LANGUAGES;
+
+  return langs.map((lang) => lang.locale);
+};
 
 const i18nDefaultOptions = (
   supportedLngs?: TSupportedLocale[]
