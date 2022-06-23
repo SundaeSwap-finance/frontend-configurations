08218ad8d4f732d60077db47bf11b541360bf2c9
akratzel <andreas.kratzel@gmail.com>
2022-06-23 12:45:39 +0200

    feat(prettier-config): add more declarations

--- packages/prettier-config/src/index.ts
+++ packages/prettier-config/src/index.ts
@@ -1,5 +1,12 @@
 module.exports = {
   // https://prettier.io/docs/en/options.html
+  // we declare some values that use the default values in order to have a better understanding of the used options
+  bracketSpacing: true,
+  bracketSameLine: false,
+  jsxSingleQuote: false,
   printWidth: 100,
-  doubleQuote: true,
+  semi: true,
+  singleQuote: false,
+  tabWidth: 2,
+  tabs: false,
 };
