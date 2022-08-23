bc803e70bf62adc798489656a1024f97f89a78bb
akratzel <andreas.kratzel@gmail.com>
2022-08-23 12:22:30 +0200

    chore(tailwind-config): add `shake` animation

--- packages/tailwind-config/src/index.ts
+++ packages/tailwind-config/src/index.ts
@@ -91,6 +91,24 @@ const theme: Omit<tailwindConfig.TailwindTheme, "keyframes"> & {
         boxShadow: "0 0 0 0 rgba(252, 165, 165, 0)",
       },
     },
+    // Shake
+    shake: {
+      "10%, 90%": {
+        transform: "translate3d(-1px, 0, 0)",
+      },
+
+      "20%, 80%": {
+        transform: "translate3d(2px, 0, 0)",
+      },
+
+      "30%, 50%, 70%": {
+        transform: "translate3d(-4px, 0, 0)",
+      },
+
+      "40%, 60%": {
+        transform: "translate3d(4px, 0, 0)",
+      },
+    },
   },
   animation: {
     ...defaultTheme.animation,
@@ -117,6 +135,8 @@ const theme: Omit<tailwindConfig.TailwindTheme, "keyframes"> & {
     "dot-flashing-dark": "dot-flashing-dark 1s infinite alternate",
     // Input
     "shadow-pulse": "shadow-pulse 0.3s forwards",
+    // Shake
+    shake: "shake 1s cubic-bezier(.36,.07,.19,.97) both",
   },
   screens: {
     xxs: "240px",
